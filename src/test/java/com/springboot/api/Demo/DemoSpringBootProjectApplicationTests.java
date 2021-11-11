4fa03d557c15879662618182d81db7080ff3b7cc
Peo <peothach@ubuntu.ubuntu-domain>
2021-11-11 09:58:33 +0700

    init project

---
+++ src/test/java/com/springboot/api/Demo/DemoSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.api.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
